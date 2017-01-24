b18e53584e962034ffab7b1b0be6d35ceee18ffd
Hayden <hayden.field@devinit.org>
2017-01-24 15:06:40 +0000

    Add codelist class docstrings

--- iati/core/codelists.py
+++ iati/core/codelists.py
@@ -1,11 +1,16 @@
 class Codelist(object):
+    """Representation of a Codelist as defined within the IATI SSOT"""
+
     def __init__(self):
         self.codes = []
         self.condition = None
         self.name = None
         self.path = None
 
+
 class Code(object):
+    """Representation of a Code contained within a Codelist"""
+
     def __init__(self):
         self.activation_date = None
         self.name = None
