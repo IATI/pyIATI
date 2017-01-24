25cd9c428a467fce92dae98476515e43806bc20c
Hayden <hayden.field@devinit.org>
2017-01-24 16:57:48 +0000

    Add codelist xml parsing TODO

--- iati/core/codelists.py
+++ iati/core/codelists.py
@@ -7,7 +7,10 @@ class Codelist(object):
 
     def __init__(self, name=None, path=None, xml=None):
         def parse_from_xml(xml):
-            """Parse a Codelist from the XML that defines it"""
+            """Parse a Codelist from the XML that defines it
+
+            TODO: Define relevant tests and add error handling
+            """
             tree = etree.fromstring(xml)
 
             self.name = tree.attrib['name']
