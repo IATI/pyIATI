169dd16e5ecd52f7658d2f4db86d0c83e15439e1
Hayden <hayden.field@devinit.org>
2017-01-24 14:48:08 +0000

    Add test stubs for codelists

---
+++ iati/core/tests/test_codelists.py
@@ -0,0 +1,9 @@
+import pytest
+
+class TestCodelists(object):
+    """A container for tests relating to codelists"""
+    pass
+
+class TestCodes(object):
+    """A container for tests relating to codes"""
+    pass
