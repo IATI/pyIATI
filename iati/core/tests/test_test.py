7fad5e545ee5ea5f0f04e8623d69b991b1e938b2
Hayden <hayden.field@devinit.org>
2017-01-24 14:39:50 +0000

    passing->running

--- iati/core/tests/test_test.py
+++ iati/core/tests/test_test.py
@@ -2,5 +2,5 @@ import pytest
 
 class TestTest():
     def test_test(self):
-        """Demonstrate that tests are passing"""
+        """Demonstrate that tests are running"""
         assert 1 == 1
