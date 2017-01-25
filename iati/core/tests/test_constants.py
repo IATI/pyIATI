3aae3f014c9c2441d8106a47566e3fd92df3ac26
Hayden <hayden.field@devinit.org>
2017-01-25 10:58:54 +0000

    Add spell checking to pylint
    And fix a typo that was caught

--- iati/core/tests/test_constants.py
+++ iati/core/tests/test_constants.py
@@ -5,7 +5,7 @@ A module containing tests for the library representation of IATI constants.
 import iati.core.constants
 
 class TestConstants(object):
-    """A containter for tests relating to IATI software constants"""
+    """A container for tests relating to IATI software constants"""
 
     def test_nsmap(self):
         """Check that the NSMAP constant exists"""
