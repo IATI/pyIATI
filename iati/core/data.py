60c0551398518ed15fc9cb45db4e834e372498fa
Hayden <hayden.field@devinit.org>
2017-02-01 10:37:35 +0000

    Add base dataset class representation

---
+++ iati/core/data.py
@@ -0,0 +1,7 @@
+"""A module containing a core representation of an IATI Dataset."""
+
+
+class Dataset(object):
+    """Representation of an IATI XML file that may be validated against a schema."""
+
+    pass
---
+++ iati/core/tests/test_data.py
@@ -0,0 +1,8 @@
+"""A module containing tests for the library representation of IATI data."""
+import iati.core.data
+
+
+class TestDatasets(object):
+    """A container for tests relating to Datasets"""
+
+    pass
