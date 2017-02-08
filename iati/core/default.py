26c36eefc9c6b342a4232fd94d39167179dec57d
Hayden <hayden.field@devinit.org>
2017-02-08 15:28:31 +0000

    Provide way to bypass codelists cache

--- iati/core/default.py
+++ iati/core/default.py
@@ -18,11 +18,12 @@ _codelists = {}
 This removes the need to repeatedly load a Codelist from disk each time it is accessed.
 """
 
-def codelists(version=0):
+def codelists(version=0, bypass_cache=False):
     """Locate the default Codelists for the specified version of the Schema.
 
     Args:
         version (float): The version of the Standard to return the Codelists for. Defaults to 0. This means that the latest version of the Codelist is returned.
+        bypass_cache (bool): Whether the cache should be bypassed, instead reloading data from disk even if it's already been loaded.
 
     Raises:
         ValueError: When a specified version is not a valid version of the IATI Standard.
@@ -33,15 +34,14 @@ def codelists(version=0):
 
     Todo:
         Actually handle versions, including errors.
-
-        Improve caching.
     """
     paths = iati.core.resources.find_all_codelist_paths()
 
     for path in paths:
-        xml_str = iati.core.resources.load_as_string(path)
         name = path.split(os.sep).pop()[:-4]
-        codelist = iati.core.codelists.Codelist(name, xml=xml_str)
-        _codelists[name] = codelist
+        if (not name in _codelists.keys()) or bypass_cache:
+            xml_str = iati.core.resources.load_as_string(path)
+            codelist = iati.core.codelists.Codelist(name, xml=xml_str)
+            _codelists[name] = codelist
 
     return _codelists
