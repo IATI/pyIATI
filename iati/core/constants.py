f811c42b4e468ab5ce8d789a2aa6a4c39bbfa6dc
Hayden <hayden.field@devinit.org>
2017-02-08 09:14:46 +0000

    Update the NSMAP and NAMESPACE

--- iati/core/constants.py
+++ iati/core/constants.py
@@ -9,9 +9,7 @@ LOG_FILE_NAME = 'iatilib.log'
 """The location of the primary IATI log file."""
 LOGGER_NAME = 'iati'
 """The name of the primary IATI Logger."""
-NSMAP = 7
-"""A hash table for interpreting namespaces in IATI Schemas.
-
-Todo:
-    Make this a valid mapping.
-"""
+NAMESPACE = '{http://www.w3.org/2001/XMLSchema}'
+"""The namespace that IATI Schema XSD files are specified within."""
+NSMAP = {'xsd': 'http://www.w3.org/2001/XMLSchema'}
+"""A dictionary for interpreting namespaces in IATI Schemas."""
--- iati/core/test/test_constants.py
+++ iati/core/test/test_constants.py
@@ -5,10 +5,11 @@ import iati.core.constants
 class TestConstants(object):
     """A container for tests relating to IATI software constants"""
 
-    def test_nsmap(self):
-        """Check that the NSMAP constant exists
+    def test_namespace(self):
+        """Check that the NAMESPACE constant is a string."""
+        assert isinstance(iati.core.constants.NAMESPACE, str)
 
-        Todo:
-            Check that NSMAP is a sensible value.
-        """
-        assert iati.core.constants.NSMAP is not None
+    def test_nsmap(self):
+        """Check that the NSMAP constant is a dictionary with the correct key."""
+        assert isinstance(iati.core.constants.NSMAP, dict)
+        assert isinstance(iati.core.constants.NSMAP['xsd'], str)
