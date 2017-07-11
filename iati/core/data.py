63321878cd51fa44f4b793ee7a445901daeba884
dalepotter <dalepotter@gmail.com>
2017-07-11 17:34:29 +0100

    Fix linting
    pylint suggests adding 4 spaces in multi-line if statement.
    This may be a false positive bug though - see:
    https://github.com/PyCQA/pylint/issues/289

--- iati/core/data.py
+++ iati/core/data.py
@@ -81,7 +81,7 @@ class Dataset(object):
 
                 # Convert the input to bytes, as etree.fromstring works most consistently with bytes objects, especially if an XML encoding declaration has been used.
                 if (isinstance(value_stripped, str)
-                    and sys.version_info.major > 2):  # Python v2 treats strings as byte objects by default
+                        and sys.version_info.major > 2):  # Python v2 treats strings as byte objects by default
                     value_stripped_bytes = value_stripped.encode()
                 else:
                     value_stripped_bytes = value_stripped
