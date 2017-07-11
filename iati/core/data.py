e64fe572816928f68f501adadc7f69e4ab7135fa
dalepotter <dalepotter@gmail.com>
2017-07-11 14:55:27 +0100

    [#24] Add logic to only convert strings to bytes in python3
    Adds additional logic statement to check python version. This is
    necessary as I couldn't find a way to make the underlaying
    functionality compatible with python2 and 3 due to the differences
    in the way both versions handle strings - i.e. strings in python2
    are treated as bytes objects by default, so converting to bytes is
    not necessary in the first place.

--- iati/core/data.py
+++ iati/core/data.py
@@ -1,4 +1,5 @@
 """A module containing a core representation of an IATI Dataset."""
+import sys
 from lxml import etree
 import iati.core.exceptions
 import iati.core.utilities
@@ -79,7 +80,8 @@ class Dataset(object):
                 value_stripped = value.strip()
 
                 # Convert the input to bytes, as etree.fromstring works most consistently with bytes objects, especially if an XML encoding declaration has been used.
-                if isinstance(value_stripped, str):
+                if (isinstance(value_stripped, str)
+                    and sys.version_info.major > 2):  # Python v2 treats strings as byte objects by default
                     value_stripped_bytes = value_stripped.encode()
                 else:
                     value_stripped_bytes = value_stripped
