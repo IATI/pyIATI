610c43a88d1b9bf3cee527358ef56ecb4640db95
dalepotter <dalepotter@gmail.com>
2017-10-16 16:17:44 +0100

    Ignore long function name when running pylint checks
    Shortening the variable name would remove consistency between this
    function and the corresponding function `get_all_activity_schema_paths`.

--- iati/core/resources.py
+++ iati/core/resources.py
@@ -139,7 +139,7 @@ def get_all_activity_schema_paths(version=None):
     return [get_schema_path(FILE_SCHEMA_ACTIVITY_NAME, version)]
 
 
-def get_all_organisation_schema_paths(version=None):
+def get_all_organisation_schema_paths(version=None):  # pylint: disable=invalid-name
     """Find the paths for all Organisation Schemas at the specified version of the Standard.
 
     Args:
