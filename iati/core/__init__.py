90307c5fa2492005d11acf77f666deb0cad95c23
Hayden <hayden.field@devinit.org>
2017-10-25 11:31:37 +0100

    Move newly created resources
    Newly created resources previously under 'core' need moving
    to the new resources folder.

--- iati/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""A package containing core IATI functionality."""
-from .codelists import Code, Codelist  # noqa: F401
-from .data import Dataset  # noqa: F401
-from .rulesets import Rule, Ruleset  # noqa: F401
-from .rulesets import RuleAtLeastOne, RuleDateOrder, RuleDependent, RuleNoMoreThanOne, RuleRegexMatches, RuleRegexNoMatches, RuleStartsWith, RuleSum, RuleUnique  # noqa: F401
-from .schemas import ActivitySchema, OrganisationSchema, Schema  # noqa: F401
