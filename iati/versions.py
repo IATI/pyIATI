3131f282d6ad1a703939c91c0d7dc0b3e4e54046
Hayden <hayden.field@devinit.org>
2018-01-29 17:14:58 +0000

    Document the current state of the Version class.

--- iati/versions.py
+++ iati/versions.py
@@ -6,7 +6,16 @@ class Version(object):
     """Representation of an IATI Standard Version Number."""
 
     def __init__(self, version_string):
-        """Initialise a Version Number."""
+        """Initialise a Version Number.
+
+        Args:
+            version_string (str): A string representation of an IATI version number.
+
+        Raises:
+            TypeError: If an attempt to pass something that is not a string is made.
+            ValueError: If a provided string is not a version number.
+
+        """
         if not isinstance(version_string, str):
             raise TypeError('A Version object must be created from a string, not a {0}'.format(type(version_string)))
 
