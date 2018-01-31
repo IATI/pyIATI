92c48c7c02d33fa058ceaa27b627787ef2dc48b3
Hayden <hayden.field@devinit.org>
2018-01-31 16:43:42 +0000

    Refactor Version.__init__ to extract string verification
    The function to check whether a value is in an IATI format is useful outside init.
    It has been extracted to both make this functionality usable elsewhere, and to improve readability.

--- iati/versions.py
+++ iati/versions.py
@@ -20,20 +20,15 @@ class Version(semantic_version.Version):
         if not isinstance(version_string, str):
             raise TypeError('A Version object must be created from a string, not a {0}'.format(type(version_string)))
 
-        # a regex for what makes a valid IATIver Version Number format string
-        iativer_re = re.compile(r'^((1\.0[1-9])|(((1\d+)|([2-9](\d+)?))\.0[1-9](\d+)?))$')
-
         # check to see if IATIver
-        if iativer_re.match(version_string):
+        if self._is_iativer(version_string):
             integer = version_string.split('.')[0]
             decimal = str(int(version_string.split('.')[1]) - 1)
             super(Version, self).__init__('.'.join([integer, decimal, '0']), True)
+        elif self._is_semver(version_string):
+            super(Version, self).__init__(version_string, True)
         else:
-            # check to see if SemVer with a positive major version
-            if semantic_version.validate(version_string) and semantic_version.Version(version_string).major != 0:
-                super(Version, self).__init__(version_string, True)
-            else:
-                raise ValueError('A valid version number must be specified.')
+            raise ValueError('A valid version number must be specified.')
 
     @property
     def integer(self):
@@ -75,6 +70,37 @@ class Version(semantic_version.Version):
             The helper methods must be used if a specific format is required.
         """
         return self.iativer_str
+
+    def _is_iativer(self, version_string):
+        """Determine whether a version string is in a IATIver format and is a permitted value.
+
+        Args:
+            version_string (string): The string to check conformance of.
+
+        Returns:
+            bool: True if the provided string is a permitted in IATIver-format version number. False if not.
+        """
+        # a regex for what makes a valid IATIver Version Number format string
+        iativer_re = re.compile(r'^((1\.0[1-9])|(((1\d+)|([2-9](\d+)?))\.0[1-9](\d+)?))$')
+
+        return iativer_re.match(version_string)
+
+    def _is_semver(self, version_string):
+        """Determine whether a version string is in a SemVer format and is a permitted value.
+
+        Args:
+            version_string (string): The string to check conformance of.
+
+        Returns:
+            bool: True if the provided string is a permitted in SemVer-format version number. False if not.
+        """
+        is_semver_format = semantic_version.validate(version_string)
+        try:
+            is_permitted_value = semantic_version.Version(version_string).major != 0
+        except ValueError:
+            return False
+
+        return is_semver_format and is_permitted_value
 
     def next_major(self):
         """Obtain a Version object that represents the next version after a Major Upgrade.
