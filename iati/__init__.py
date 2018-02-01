3690be2d6ee15bf59126e0c63583227ac0c56b97
Hayden <hayden.field@devinit.org>
2018-02-01 10:16:01 +0000

    Fix import error
    Version has to be imported before Codelists due to import order meaning Codelists depends on Version.

--- iati/__init__.py
+++ iati/__init__.py
@@ -1,9 +1,9 @@
 """A top-level namespace package for IATI."""
+from .versions import Version  # noqa: F401
 from .codelists import Code, Codelist  # noqa: F401
 from .data import Dataset  # noqa: F401
 from .rulesets import Rule, Ruleset  # noqa: F401
 from .rulesets import RuleAtLeastOne, RuleDateOrder, RuleDependent, RuleNoMoreThanOne, RuleRegexMatches, RuleRegexNoMatches, RuleStartsWith, RuleSum, RuleUnique  # noqa: F401
 from .schemas import ActivitySchema, OrganisationSchema  # noqa: F401
-from .versions import Version  # noqa: F401
 
 __import__('pkg_resources').declare_namespace(__name__)
