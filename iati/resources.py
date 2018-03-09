a3f8112532f505296f207881a17b834fab187e6c
Hayden <hayden.field@devinit.org>
2018-03-09 12:18:51 +0000

    Actually save a file between resolving a merge conflict and committing
    Oops...

--- iati/resources.py
+++ iati/resources.py
@@ -308,10 +308,7 @@ def create_schema_path(name, version=iati.version.STANDARD_VERSION_ANY):
     return path_for_version(os.path.join(PATH_SCHEMAS, '{0}'.format(name) + FILE_SCHEMA_EXTENSION), version)
 
 
-<<<<<<< HEAD
 @iati.version.allow_possible_version
-=======
->>>>>>> improve-version-parameter
 @iati.version.normalise_decimals
 def folder_name_for_version(version=iati.version.STANDARD_VERSION_ANY):
     """Return the folder name for a given version of the Standard.
