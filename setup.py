c33e9942baceec221e01da62f570700b5e962531
requires.io <support@requires.io>
2018-07-14 14:53:12 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         # XML handling library
         'lxml==4.2.3',
         # YAML parsing for validation error codes
-        'PyYAML==4.1',
+        'PyYAML==3.13',
         # SemVer library
         'semantic_version==2.6.0'
         ],
