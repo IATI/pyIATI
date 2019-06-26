d7485baa1bc8634779eca24e6aef0d4eba97773f
dalepotter <dalepotter@gmail.com>
2019-06-26 08:53:01 +0100

    Update setup file to include python 3.7

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Markup :: XML'
